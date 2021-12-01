8e6a1d17323045b4a3b641619535bdc7d13e22b4
Sora <dream@neoncity.dev>
2021-12-01 13:43:46 +0100

    Made more idiomatic, solved part2

--- src/day1.rs
+++ src/day1.rs
@@ -1,16 +1,20 @@
 use aoc_runner_derive::aoc;
 
 #[aoc(day1, part1)]
-pub fn day1(input: &str) -> u32 {
-    let mut counter = 0u32;
-    let mut last_depth = 0u32;
+pub fn day1_part1(input: &str) -> usize {
+    let depths: Vec<u32> = input.lines().map(|x| x.parse().unwrap()).collect();
+    
+    depths
+        .windows(2)
+        .filter(|x| x[0] < x[1])
+        .count()
+}
 
-    for depth in input.lines() {
-        let cur_depth = depth.parse().unwrap();
-        if last_depth > 0 && cur_depth > last_depth {
-            counter += 1u32;
-        }
-        last_depth = cur_depth;
-    }
-    counter
+#[aoc(day1, part2)]
+pub fn day1_part2(input: &str) -> usize {
+    let depths: Vec<u32> = input.lines().map(|x| x.parse().unwrap()).collect();
+
+    let summarized: Vec<u32> = depths.windows(3).map(|x| x[0] + x[1] + x[2]).collect();
+
+    summarized.windows(2).filter(|x| x[0] < x[1]).count()
 }
